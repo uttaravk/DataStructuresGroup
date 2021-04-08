f9898040ce5990bd73c2b4ccd3161e8cfc76baa8
Uttara Kulkarni <ukulkarni@ebay.com>
2021-04-07 20:45:44 -0700

    added two sum solution 2

--- two-sum/Uttara/src/TwoSumSolution.java
+++ two-sum/Uttara/src/TwoSumSolution.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSumSolution {
 
@@ -8,17 +10,34 @@ public class TwoSumSolution {
         int[] targets = new int[]{9, 6, 6};
 
         for (int i = 0; i < intArrays.length; i++) {
-            System.out.println(Arrays.toString(twoSumSolution.twoSum(intArrays[i], targets[i])));
+            System.out.println(Arrays.toString(twoSumSolution.twoSum1(intArrays[i], targets[i])));
+            System.out.println(Arrays.toString(twoSumSolution.twoSum2(intArrays[i], targets[i])));
         }
 
     }
 
-    public int[] twoSum(int[] nums, int target) {
+
+    //Brute Force Solution
+    public int[] twoSum1(int[] nums, int target) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (target - nums[i] == nums[j]) {
                     return new int[]{i, j};
                 }
+            }
+        }
+        return null;
+    }
+
+
+    //Optimized Solution
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> mapnums = new HashMap<>();
+        for (int pos = 0; pos < nums.length; pos++) {
+            if (mapnums.containsKey(target - nums[pos])) {
+                return new int[]{pos, mapnums.get(target - nums[pos])};
+            } else {
+                mapnums.put(nums[pos], pos);
             }
         }
         return null;
